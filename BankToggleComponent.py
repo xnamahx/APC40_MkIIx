b7daa674ce23d8d39c5eb7207771ac9dc87eba37
xnamahx@gmail.com <xnamahx@gmail.com>
2018-06-23 11:48:33 -0300

    Initial Commit exact copy from https://github.com/gluon/AbletonLive10_MIDIRemoteScripts/tree/master/APC40_MkII

---
+++ BankToggleComponent.py
@@ -0,0 +1,24 @@
+# Embedded file name: /Users/versonator/Jenkins/live/output/mac_64_static/Release/python-bundle/MIDI Remote Scripts/APC40_MkII/BankToggleComponent.py
+# Compiled at: 2018-04-23 20:27:04
+from __future__ import absolute_import, print_function, unicode_literals
+from _Framework.ComboElement import ToggleElement
+from _Framework.Control import ToggleButtonControl
+from _Framework.ControlSurfaceComponent import ControlSurfaceComponent
+
+class BankToggleComponent(ControlSurfaceComponent):
+    bank_toggle_button = ToggleButtonControl()
+
+    def __init__(self, *a, **k):
+        super(BankToggleComponent, self).__init__(*a, **k)
+        self._toggle_elements = []
+
+    @bank_toggle_button.toggled
+    def bank_toggle_button(self, toggled, button):
+        for e in self._toggle_elements:
+            e.set_toggled(toggled)
+
+    def create_toggle_element(self, *a, **k):
+        element = ToggleElement(*a, **k)
+        element.toggled = self.bank_toggle_button.is_toggled
+        self._toggle_elements.append(element)
+        return element
---
+++ __init__.py
@@ -0,0 +1,16 @@
+# Embedded file name: /Users/versonator/Jenkins/live/output/mac_64_static/Release/python-bundle/MIDI Remote Scripts/APC40_MkII/__init__.py
+# Compiled at: 2018-04-23 20:27:04
+from __future__ import absolute_import, print_function, unicode_literals
+from _Framework.Capabilities import CONTROLLER_ID_KEY, PORTS_KEY, NOTES_CC, SCRIPT, SYNC, REMOTE, controller_id, inport, outport
+from .APC40_MkII import APC40_MkII
+
+def create_instance(c_instance):
+    return APC40_MkII(c_instance)
+
+
+def get_capabilities():
+    return {CONTROLLER_ID_KEY: controller_id(vendor_id=2536, product_ids=[
+                         41], model_name='Akai APC40 MkII'), 
+       PORTS_KEY: [
+                 inport(props=[NOTES_CC, SCRIPT, REMOTE]),
+                 outport(props=[SYNC, SCRIPT, REMOTE])]}
